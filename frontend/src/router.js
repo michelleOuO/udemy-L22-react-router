6ec70891f2c7f72a8408c1ecba85f3604bf40438
Michelle Hung <michelle.hung@outland.tw>
2026-03-03 16:02:15 +0800

    feat: add loader

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+// import EventsPage from "./pages/EventsPage";
+import EventsPage, { eventsLoader } from "./pages/Event";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -17,7 +18,11 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRoot />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
           { path: ":id", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":id/edit", element: <EditEventPage /> },
