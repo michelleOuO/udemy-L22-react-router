a37c62cc6e1a1f9d2205cc4b0ea1ea7b43f9f5ba
Michelle Hung <michelle.hung@outland.tw>
2026-03-05 15:18:45 +0800

    feat: add eventDetailLoader

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,14 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const { id } = useParams();
+  const { event } = useLoaderData();
 
-  return (
-    <div>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {id}</p>
-    </div>
-  );
+  return <EventItem event={event} />;
 }
 
 export default EventDetailPage;
+
+export async function eventDetailLoader({ request, params }) {
+  console.log("request", request);
+  console.log("params", params);
+
+  const id = params.id;
+  const response = await fetch(`http://localhost:8080/events/${id}`);
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: "Could not fetch details for selected event.",
+      }),
+      {
+        status: 500,
+      },
+    );
+  } else {
+    return response;
+  }
+}
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 // import EventsPage from "./pages/EventsPage";
 import EventsPage, { eventsLoader } from "./pages/Event";
-import EventDetailPage from "./pages/EventDetailPage";
+import EventDetailPage, { eventDetailLoader } from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import Root from "./pages/Root";
@@ -25,7 +25,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":id", element: <EventDetailPage /> },
+          {
+            path: ":id",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":id/edit", element: <EditEventPage /> },
         ],
